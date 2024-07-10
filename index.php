36113c3d09245b089ed8469eefccb1c4d1b9188f
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2024-07-10 18:18:45 +0530

    Added css file

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Home</title>
 </head>
 <body>
-    This is home
+    <main>
+        <nav class="nav-container">
+        <img src="#" alt="College Name / Logo">
+            <ul>
+                <li>dashboard</li>
+            </ul>
+        </nav>
+    </main>
 </body>
 </html>
